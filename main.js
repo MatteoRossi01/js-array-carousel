62a8060129cef4f0cca1abc7125c0af38714d3ab
Matteo Rossi <matteorossii6920@gmail.com>
2022-01-27 16:05:02 +0100

    Completamento parziale dell'esercizio

--- main.js
+++ main.js
@@ -22,9 +22,36 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
+const itemsRef = document.getElementsByClassName('container-big')[0];
+const thumbsRef = document.getElementsByClassName('container-small')[0];
+
+let col = '';
+let imgPrincipale = '';
+let active = 1;
 
 for (let i = 0; i < items.length; i++) {
 
-    
+    /* Posizionameno slide dinamico */
+    col += 
+            `<div class="col">
+                <img id="select" class="img-small" src="${items[i]}">
+            </div>`;
 
+    /* Posizionamento info e immagine dinamico */
+    imgPrincipale +=
+                    `<div class="item">
+                        <img class="img-big" src="${items[i]}" alt="">
+                        <div class="col-big">
+                            <h2 class="title-img">${title[i]}</h2>
+                            <span class="info-img">${text[i]}</span>
+                        </div> 
+                    </div>`;                
 }
+
+//creo le slide
+thumbsRef.innerHTML = col;
+document.getElementsByClassName('col')[active].classList.add('active');
+
+//creo la sezione princiale
+itemsRef.innerHTML = imgPrincipale;
+document.getElementsByClassName('item')[active].classList.add('active');
